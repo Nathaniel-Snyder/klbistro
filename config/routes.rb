56ea6028294073971bbbf7ef671b5b85674b5096
Nathaniel-Snyder <nrsnyder8@gmail.com>
2016-01-06 12:12:06 -0600

    Navbar and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get '/menus'      => 'static_pages#menus',      as: 'menus'
+  get '/directions' => 'static_pages#directions', as: 'directions'
+  get '/press'      => 'static_pages#press',      as: 'press'
+  get '/events'     => 'static_pages#events',     as: 'events'
+  get '/contact'    => 'static_pages#contact',    as: 'contact'
+  
 end
