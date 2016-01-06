67a7eccf19d74425b8d72834bc77f05dd7866cbc
Nathaniel-Snyder <nrsnyder8@gmail.com>
2016-01-06 10:28:15 -0600

    Set root route

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,21 @@
+class StaticPagesController < ApplicationController
+
+  def home
+  end
+
+  def contact
+  end
+
+  def menus
+  end
+
+  def directions
+  end
+
+  def press
+  end
+
+  def events
+  end
+  
+end
